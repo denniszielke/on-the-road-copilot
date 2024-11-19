efd8c03350646121c1b17ab01b1ac0f6cdf4ca2b
denniszielke <11569044+denniszielke@users.noreply.github.com>
2024-11-19 22:37:03 +0100

    updated phone receiver handler

--- src/app/acs/caller.py
+++ src/app/acs/caller.py
@@ -2,7 +2,7 @@
 from logging import INFO
 from azure.eventgrid import EventGridEvent, SystemEventNames
 from azure.core.messaging import CloudEvent
-
+from typing import List, Optional, Union, TYPE_CHECKING
 from azure.communication.callautomation import (
     CallAutomationClient,
     CallConnectionClient,
@@ -12,8 +12,11 @@ from azure.communication.callautomation import (
     CallInvite,
     RecognitionChoice,
     DtmfTone,
+    VoiceKind,
+    FileSource,
     TextSource)
-
+import json
+import requests
 
 class OutboundCall:
     target_number: str
@@ -28,24 +31,27 @@ class OutboundCall:
         self.acs_callback_path = acs_callback_path
     
     async def call(self):
-        call_automation_client = CallAutomationClient.from_connection_string(self.acs_connection_string)
-        target_participant = PhoneNumberIdentifier(self.target_number)
-        source_caller = PhoneNumberIdentifier(self.source_number)
-        call_connection_properties = call_automation_client.create_call(target_participant, 
+        self.call_automation_client = CallAutomationClient.from_connection_string(self.acs_connection_string)
+        self.target_participant = PhoneNumberIdentifier(self.target_number)
+        self.source_caller = PhoneNumberIdentifier(self.source_number)
+        call_connection_properties = self.call_automation_client.create_call(self.target_participant, 
                                                                     self.acs_callback_path,
-                                                                    source_caller_id_number=source_caller)
+                                                                    source_caller_id_number=self.source_caller)
 
-    def _outbound_call_handler():
+    async def _outbound_call_handler(self, request):
         print("Outbound call handler")
-        for event_dict in request.json:
+        cloudevent = await request.json()
+        for event_dict in cloudevent:
             # Parsing callback events
             event = CloudEvent.from_dict(event_dict)
             call_connection_id = event.data['callConnectionId']
             print("%s event received for call connection id: %s", event.type, call_connection_id)
             call_connection_client = self.call_automation_client.get_call_connection(call_connection_id)
-            target_participant = PhoneNumberIdentifier(self.target_number)
+            # target_participant = PhoneNumberIdentifier(self.target_number)
             if event.type == "Microsoft.Communication.CallConnected":
                 print("Call connected")
+                print(call_connection_id)
+
 
     def attach_to_app(self, app, path):
-        app.router.add_get(path, self._outbound_call_handler)
+        app.router.add_post(path, self._outbound_call_handler)
